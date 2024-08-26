3f18860df386ae601b2dc9f5fc48b44eec284b0c
Jean Lucas <j3anlucas010@gmail.com>
2024-08-26 10:07:03 -0300

    Click sobre mim aparece no index

--- src/script/script.js
+++ src/script/script.js
@@ -1,14 +1,35 @@
 const btn = document.querySelector(".btn");
 const contatos = document.querySelector("#contato");
 
+
+// Entre em Contato
 function aparecer() {
   btn.addEventListener("click", () => {
     if (contatos.style.opacity == "0") {
       contatos.style.opacity = "1";
+      btn.classList.add('btnclq')
+
     } else {
       contatos.style.opacity = "0";
+      btn.classList.remove('btnclq')
+
     }
+
   });
 }
 aparecer();
 
+// sobreM
+const texto=document.querySelector('.texto-sobre')
+const sobre=document.querySelector('.sobreM')
+sobre.addEventListener('click',()=>{
+if(texto.style.display=='block'){
+texto.style.display='none'
+}
+else{
+  texto.style.display='block'
+
+}
+
+
+})
