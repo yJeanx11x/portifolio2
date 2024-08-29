9545d5314d9a571b5b36417f17f284d025fc5bb1
Jean Lucas <j3anlucas010@gmail.com>
2024-08-29 11:05:35 -0300

    box de projeto adicionado

--- src/script/script.js
+++ src/script/script.js
@@ -1,35 +1,58 @@
-const btn = document.querySelector(".btn");
-const contatos = document.querySelector("#contato");
+// sobreM
+const texto = document.querySelector(".texto-sobre");
+const sobre = document.querySelector(".sobreM");
+sobre.addEventListener("click", () => {
+  if (texto.style.display == "block") {
+    texto.style.display = "none";
+    texto.innerHTML = `Meu Nome E jean Lucas`;
+  } else {
+    texto.style.display = "block";
+  }
+});
 
+// formacao
+function formacao() {
+  const formacao = document.querySelector(".formacao");
+  const formacaotxt = document.querySelector(".formacaotxt");
+
+  formacao.addEventListener("click", () => {
+    if (texto.style.display == "block") {
+      texto.style.display = "none";
+      texto.innerHTML = "Formação";
+    } else {
+      texto.style.display = "block";
+    }
+  });
+}
+formacao();
+// Projetos
+function projeto() {
+  const projeto = document.querySelector(".projetos");
+  const box = document.querySelector(".box");
+
+  projeto.addEventListener("click", () => {
+    if (box.style.opacity == "0") {
+      box.style.opacity = "1";
+    } else {
+      box.style.opacity = "0";
+    }
+  });
+}
+
+projeto();
 
 // Entre em Contato
 function aparecer() {
+  const btn = document.querySelector(".btn");
+  const contatos = document.querySelector("#contato");
   btn.addEventListener("click", () => {
     if (contatos.style.opacity == "0") {
       contatos.style.opacity = "1";
-      btn.classList.add('btnclq')
-
+      btn.classList.add("btnclq");
     } else {
       contatos.style.opacity = "0";
-      btn.classList.remove('btnclq')
-
+      btn.classList.remove("btnclq");
     }
-
   });
 }
 aparecer();
-
-// sobreM
-const texto=document.querySelector('.texto-sobre')
-const sobre=document.querySelector('.sobreM')
-sobre.addEventListener('click',()=>{
-if(texto.style.display=='block'){
-texto.style.display='none'
-}
-else{
-  texto.style.display='block'
-
-}
-
-
-})
